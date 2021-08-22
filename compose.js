6c6b780cbbd5fe4f708e7ffb3391f6fe01cb7d1f
bad4iz <developer@bad4iz.ru>
2021-08-22 09:26:27 +0400

    compose #3

--- compose.js
+++ compose.js
@@ -1,17 +1,8 @@
-
-const compose = (...fns) => (...arg) => {
-    if(fns.length < 2) {
-        return fns[0](...arg)
-    }
-    const firstFn = fns.shift();
-    return firstFn(compose(...fns)(...arg));
-}
-const fn1 = (a,b, c) => a+ b + c;
-const fn2 = a => a*2;
-const fn3 = a => a+3;
-compose( fn3, fn2, fn1)(2, 3, 1); //?
-
-//
+/**
+ * 1 реализация
+ * @param fns
+ * @returns {function(...[*]): *}
+ */
 // const compose = (...fns) => (...args) => {
 //     const lastFn = fns.pop();
 //
@@ -19,23 +10,31 @@ compose( fn3, fn2, fn1)(2, 3, 1); //?
 //       return fn(composed)
 //     },lastFn(...args)) //?
 // };
-//
 
 
 
+/**
+ * 2 реализация
+ * @param fns
+ * @returns {function(...[*]): *}
+ */
+// const compose = (...fns) => (...arg) => {
+//     if(fns.length < 2) {
+//         return fns[0](...arg)
+//     }
+//     const firstFn = fns.shift();
+//     return firstFn(compose(...fns)(...arg));
+// }
+
+/**
+ * 3 реализация
+ * @param fns
+ * @returns {function(...[*]): *}
+ */
+// const compose = (...fns) => fns.reduceRight((acc, curr)=>(...args)=>curr(acc(...args)))
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const fn1 = (a, b, c) => a + b + c;
+const fn2 = a => a*2;
+const fn3 = a => a+3;
+compose( fn3, fn2, fn1)(2, 3, 1); //?
